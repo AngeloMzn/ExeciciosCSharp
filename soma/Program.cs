dfec23831ebc19f428de51c9323fd696c4d8d3a8
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-01-06 18:49:21 -0300

    Add files via upload

---
+++ soma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int x, y, soma;
+                
+                System.Console.WriteLine("Digite o valor de X:");
+                x = int.Parse(Console.ReadLine());
+
+                System.Console.WriteLine("Digite o valor de Y:");
+                y = int.Parse(Console.ReadLine());
+
+                soma = x + y;
+
+                System.Console.WriteLine("SOMA = " + soma);
+
+        }
+    }
+}
