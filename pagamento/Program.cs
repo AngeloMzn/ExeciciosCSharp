6ae824ad2c488f9143d8da48b6fc2733160ebeae
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-01-06 18:48:23 -0300

    Add files via upload

---
+++ pagamento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pagamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int horaTrab;
+            double valorHora, salario;
+
+            System.Console.WriteLine("Nome:");
+            nome = Console.ReadLine();
+
+            System.Console.WriteLine("Valor por hora:");
+            valorHora = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Horas trabalhadas:");
+            horaTrab = int.Parse(Console.ReadLine());
+
+            salario = (double)valorHora * horaTrab;
+
+            System.Console.WriteLine("O pagamento para " + nome + " deve ser " + salario.ToString("F2"));
+
+        }
+    }
+}
