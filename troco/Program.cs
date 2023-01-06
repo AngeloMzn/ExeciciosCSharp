bac56867e35c94270d28a558862d11d308fff6ab
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-01-06 18:50:24 -0300

    Add files via upload

---
+++ troco/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace troco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdComprada;
+            double precoUni, valorRecebido, precoTotal, troco;
+            
+            System.Console.WriteLine("Preço unitário do produto:");
+            precoUni = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Quantidade comprada:");
+            qtdComprada = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Dinheiro recebido:");
+            valorRecebido = double.Parse(Console.ReadLine());
+
+            precoTotal = precoUni * qtdComprada;
+
+            troco = valorRecebido - precoTotal;
+
+            System.Console.WriteLine("TROCO = " + troco.ToString("F2"));
+
+        }
+    }
+}
