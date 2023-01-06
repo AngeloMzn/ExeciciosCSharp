2da16c1a8352ff92e13bf1e058dd06f308f2c1be
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-01-06 18:49:52 -0300

    Add files via upload

---
+++ terreno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace terreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double larg, comp, valorMM, area, preco;
+            
+            System.Console.WriteLine("Digite a largura do terreno:");
+            larg = double.Parse(Console.ReadLine());
+            
+            System.Console.WriteLine("Digite o comprimento do terreno:");
+            comp = double.Parse(Console.ReadLine());
+            
+            System.Console.WriteLine("Digite o valor do metro quadrado:");
+            valorMM = double.Parse(Console.ReadLine());
+
+            
+            area = larg * comp;
+            preco = valorMM * area;
+
+            System.Console.WriteLine("preco do terreno = " + preco.ToString("F2"));
+
+        }
+    }
+}
